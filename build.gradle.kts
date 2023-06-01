b28bdc75b944ab2c3492f74334008e039390d595
dwancordoba <dwanzcc@gmail.com>
2023-06-01 16:23:03 -0500

    hook test

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.21" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
+    //id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
 }
 
 buildscript {
@@ -19,5 +20,13 @@ allprojects {
         google()
         mavenCentral()
     }
+    tasks.register<Copy>("installGitHook") {
+        from(File(rootProject.rootDir, "pre-commit"))
+        into(File(rootProject.rootDir, ".git/hooks"))
+        fileMode = 777
+    }
+
+
 }
+tasks.getByPath(":app:preBuild").dependsOn("installGitHook")
 
